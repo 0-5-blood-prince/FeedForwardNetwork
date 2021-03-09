91a618db39a1e95c9e0774712b96f149358fdb53
Mooizz <albusdumbledore8918@gmail.com>
2021-03-10 00:10:33 +0530

    Question 1 first commit

---
+++ src/dataset.py
@@ -0,0 +1,33 @@
+import os
+import wandb
+from keras.datasets import fashion_mnist 
+from keras.utils import np_utils
+labels = ["Tshirt","Trouser","Pullover","Dress","Coat","Sandal","SHirt","Sneaker","Bag","Boot"]
+def load():
+  (x_train,y_train),(x_test,y_test) = fashion_mnist.load_data()
+  x_train, x_val = x_train[:50000], x_train[50000:]
+  y_train, y_val = y_train[:50000], y_train[50000:]
+  return {
+      'x_train' : x_train,
+      'y_train' : y_train,
+      'x_val' : x_val,
+      'y_val' : y_val,
+      'x_test' : x_test,
+      'y_test' : y_test,
+  }
+def log_images():
+  dataset = load()
+  train_images = dataset['x_train']
+  train_labels = dataset['y_train']
+  set_images = []
+  set_labels = []
+  for i in range(len(train_images)):
+    if len(set_labels) == 10:
+      break
+    if labels[train_labels[i]] not in set_labels:
+      set_images.append(train_images[i])
+      set_labels.append(labels[train_labels[i]])
+  wandb.log({"Examples" : [wandb.Image(img,caption = caption) for img,caption in zip(set_images, set_labels)]})
+############################# Checking ############################
+wandb.init(project = 'dummy_DL')
+log_images()
