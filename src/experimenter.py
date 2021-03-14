f7fd64920abde917d6f5e9ce1d2f7efcef46d041
Jaitesh Pachipulusu <jaiteshp@gmail.com>
2021-03-14 15:30:51 +0530

    Added backward Prop(not working)

--- src/experimenter.py
+++ src/experimenter.py
@@ -49,7 +49,7 @@ sweep_config = {
     }
 }
 # swwep_id = wandb.sweep(sweep_config, entity="sweep",project="testing")
-network = nn.NeuralNet(3,[input_dim,4,4,4,num_classes],['relu','relu','relu','soft_max'])
+network = nn.NeuralNet(3,[input_dim,64,64,64,num_classes],['relu','relu','relu','soft_max'])
 decay = 0
-network.fit(X_train, X_val, Y_train, Y_val, 1000,'cross_entropy', 0.001, 0.9, 0, 'gd' )
+network.fit(X_train, X_val, Y_train, Y_val, 128,'cross_entropy', 0.001, 0.9, 0, 'sgd' )
 # Configure the sweep – specify the parameters to search through, the search strategy, the optimization metric et all.
