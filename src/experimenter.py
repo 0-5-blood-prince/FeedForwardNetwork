ddf37d14cbe4f6560031ca57f912af46febd7ce3
Mooizz <albusdumbledore8918@gmail.com>
2021-03-14 16:00:13 +0530

    Added Train Function

--- src/experimenter.py
+++ src/experimenter.py
@@ -25,7 +25,7 @@ sweep_config = {
         'num_hidden_layers': {
             'values': [3,4,5]
         },
-        'hidden_layer_size': {
+        'hidden_layer_size': {  
             'values': [32,64,128]
         },
         'weight_decay': {
@@ -48,8 +48,36 @@ sweep_config = {
         }
     }
 }
+
 # swwep_id = wandb.sweep(sweep_config, entity="sweep",project="testing")
-network = nn.NeuralNet(3,[input_dim,4,4,4,num_classes],['relu','relu','relu','soft_max'])
+def train():
+    config_defaults = {
+        'epochs' : 5,
+        'batch_size' : 64,
+        'weight_decay' : 0.0005,
+        'learning_rate' : 1e-3,
+        'activation' : 'relu',
+        'optimizer' : 'sgd',
+        'num_hidden_layers' : 3,
+        'hidden_layer_size' : 128,
+        'momentum' : 0.9
+    }
+    wandb.init(config=config_defaults)
+    config = wandb.config
+    sizes = [input_dim]
+    activations = []
+    for i in range(config.num_hidden_layers):
+        sizes.append(config.hidden_layer_size)
+        activations.append(config.activation)
+    sizes.append(num_classes)
+    activations.append('soft_max')
+
+    network = nn.NeuralNet(config.num_hidden_layers,sizes,activations)
+    network.fit(X_train, X_val , Y_train, Y_val, config.batch_size ,'cross_entropy',config.learning_rate,
+     config.momentum ,config.weight_decay ,config.optimizer)
+    
+network = nn.NeuralNet(3,[input_dim,128,128,128,num_classes],['relu','relu','relu','soft_max'])
 decay = 0
-network.fit(X_train, X_val, Y_train, Y_val, 1000,'cross_entropy', 0.001, 0.9, 0, 'gd' )
+network.fit(X_train, X_val, Y_train, Y_val, 128,'cross_entropy', 0.001, 0.9, 0, 'momentum' )
 # Configure the sweep – specify the parameters to search through, the search strategy, the optimization metric et all.
+### wandb.agent(sweep_id, train)
