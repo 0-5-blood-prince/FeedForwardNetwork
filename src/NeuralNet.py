Softmax, dataset fixed

src/NeuralNet.py
@@ -44,16 +44,16 @@ class NeuralNet:
         ## check if this works for a vector
         # return 1/(1 + np.exp(-x))
     ### Output Activations ###
-    def softmax(self, x):
-        return np.exp(x - sc.logsumexp(x))
-    # def softmax(self, a):
-        
-    #     ### checking ##
-        # return sm(a)
-    #     # assert(a.shape[1]==10)
-    #     # e = np.exp(a)
-    #     # return e / np.sum(e, axis=1, keepdims=True)
-
+    def softmax(self, z):
+        assert len(z.shape) == 2
+        s = np.max(z, axis=1)
+        s = s[:, np.newaxis] # necessary step to do broadcasting
+        e_x = np.exp(z - s)
+        div = np.sum(e_x, axis=1)
+        div = div[:, np.newaxis] # dito
+        output = e_x / div
+        return output
+    
     def activate(self, activation, x):
         if activation == 'relu':
             # print(x)
@@ -160,8 +160,8 @@ class NeuralNet:
             # print(fx)
             # print(y)
             # print(l)
-            dw[l-1] = (1/num_samples)*np.matmul(da[l],h)
-            db[l-1] = (1/num_samples)*np.sum(da[l], axis = 1, keepdims=True)
+            dw[l-1] = np.matmul(da[l],h)
+            db[l-1] = np.sum(da[l], axis = 1, keepdims=True)
             # print(W[l-1].T.shape, da[l].shape, l)
             dh = np.matmul(W[l-1].T, da[l])
             hadamardProd = self.activation_grads(activations, self.aggLayer[l-1].T)
@@ -230,8 +230,8 @@ class NeuralNet:
                 elif loss_fn == 'square_error':
                     loss += mean_squared_error(mini_output,y_hat)
                 
-            print('W', self.W)
-            print('b', self.b)
+            # print('W', self.W)
+            # print('b', self.b)
             return loss
 
     def fit(self, train_inputs, valid_inputs, train_outputs, valid_outputs, batch_size, loss_fn, eta, gamma, decay, optimizer):

src/dataset.py
@@ -35,17 +35,20 @@ def dataset():
     for x in X:
       a.append((np.asarray(x)).flatten())
     return np.asarray(a)
+  def normalize(X):
+    return np.multiply(1/255,X)
   data = load()
-  X_train = flat(data['x_train'])
-  X_val = flat(data['x_val'])
+  X_train = normalize(flat(data['x_train']))
+  X_val = normalize(flat(data['x_val']))
   Y_train = np.eye(10)[data['y_train']]
   Y_val = np.eye(10)[data['y_val']]
+  ## (50k,784) (50k,10)
   return {
       'x_train' : X_train,
       'y_train' : Y_train,
       'x_val' : X_val,
       'y_val' : Y_val,
-      'x_test' : data['x_test'],
+      'x_test' : normalize(data['x_test']),
       'y_test' : data['y_test'],
   }
 # def flat(X):

src/experimenter.py
@@ -50,7 +50,7 @@ sweep_config = {
 }
 # swwep_id = wandb.sweep(sweep_config, entity="sweep",project="testing")
 # network = nn.NeuralNet(1,[input_dim,32,32,32,num_classes],['relu','relu','relu','soft_max'])
-network = nn.NeuralNet(1,[input_dim,4,num_classes],['sigmoid','soft_max'])
+network = nn.NeuralNet(3,[input_dim,128,128,128,num_classes],['tanh','tanh','tanh','soft_max'])
 decay = 0
-network.fit(X_train, X_val, Y_train, Y_val, 32,'cross_entropy', 0.01, 0.9, 0, 'sgd' )
+network.fit(X_train, X_val, Y_train, Y_val, 128,'cross_entropy', 1e-3, 0.9, 0, 'sgd' )
 # Configure the sweep – specify the parameters to search through, the search strategy, the optimization metric et all.